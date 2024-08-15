d8a128f6a6be74c323da57ca9a7f27dabf469782
mohammedsaif-fb <mohammed.saif@fishbonesolutions.co.uk>
2024-08-15 17:43:32 +0100

    Update Error

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,9 +1,8 @@
 import React from "react";
-
 const ErrorComponent = () => {
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
-      <h1>Oops! Something went wrong.</h1>
+      <h1>Something went wrong.</h1>
       <p>We encountered an unexpected error. Please try again later.</p>
     </div>
   );
