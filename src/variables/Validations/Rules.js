803aa16e10f056b1b2cdf5ec74a652491acb1d99
touqeer881 <touqeer.hussain@fishbonesolutions.co.uk>
2024-10-18 18:48:02 +0100

    add space in the regax and pattern

--- src/variables/Validations/Rules.js
+++ src/variables/Validations/Rules.js
@@ -1,22 +1,22 @@
 export const validationRules = {
   numericPlus: {
-    pattern: /^[0-9+]*$/,
-    regex: /[^0-9+]/g,
+    pattern: /^[0-9+ ]*$/, // Allows numbers, +, and spaces
+    regex: /[^0-9+ ]/g,
   },
   alphaNumeric: {
-    pattern: /^[a-zA-Z0-9]*$/,
-    regex: /[^a-zA-Z0-9]/g,
+    pattern: /^[a-zA-Z0-9 ]*$/, // Allows letters, numbers, and spaces
+    regex: /[^a-zA-Z0-9 ]/g,
   },
   alphaNumericDash: {
-    pattern: /^[a-zA-Z0-9-]*$/,
-    regex: /[^a-zA-Z0-9-]/g,
+    pattern: /^[a-zA-Z0-9- ]*$/, // Allows letters, numbers, -, and spaces
+    regex: /[^a-zA-Z0-9- ]/g,
   },
   alphaNumericSlash: {
-    pattern: /^[a-zA-Z0-9/-]*$/,
-    regex: /[^a-zA-Z0-9/-]/g,
+    pattern: /^[a-zA-Z0-9/ ]*$/, // Allows letters, numbers, /, and spaces
+    regex: /[^a-zA-Z0-9/ ]/g,
   },
   alphaNumericDashSlash: {
-    pattern: /^[a-zA-Z0-9-/]*$/,
-    regex: /[^a-zA-Z0-9-/]/g,
+    pattern: /^[a-zA-Z0-9-/ ]*$/, // Allows letters, numbers, -, /, and spaces
+    regex: /[^a-zA-Z0-9-/ ]/g,
   },
 };
