58998e6a9069d0e6250d45d426533d477e7c7f0c
touqeer881 <touqeer.hussain@fishbonesolutions.co.uk>
2024-10-28 11:02:55 +0000

    multiple changes

--- src/views/pages/item_request/index.jsx
+++ src/views/pages/item_request/index.jsx
@@ -1,7 +1,15 @@
+import FeatureNotAvailabe from "components/svg/FeatureNotAvailabe";
 import React from "react";
 
 const ItemRequest = () => {
-  return <div className="flex flex-grow-0 justify-content-center"><h2 className="mt-4 text-center">This page is underconstruction.</h2></div>;
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <FeatureNotAvailabe width="120" height="120" />
+      <h2 className="mt-4 text-center">
+        This feature is not available in BETA Release
+      </h2>
+    </div>
+  );
 };
 
 export default ItemRequest;
